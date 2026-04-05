3306ca5ef704f084b4957a94f187efab3540cbdf
slchin-su <slchin@syr.edu>
2026-04-05 12:25:08 -0400

    project2updates

--- project02/chin-project02.js
+++ project02/chin-project02.js
@@ -1,13 +1,13 @@
 const objectIds = [
    437986, //Denial of St. Peter, Caravaggio x
-   436140, //Dancers, Pink and Green, Edgar Degas ?
+   436140, //Dancers, Pink and Green, Edgar Degas x
    437519, //A River Landscape, Théodore Rousseau x
    437392,//Herman Doomer, Rembrandt x
    437878, //A Maid Asleep, Vermeer x
    436526, //First Steps, after Millet, Vincent van Gogh x
    435868, //The Card Players, Paul Cezanne x
    435702, //The Horse Fair, Rosa Bonheur x
-   438123, //A Man Leaning on a Parapet, Georges Seurat ?
+   438123, //A Man Leaning on a Parapet, Georges Seurat x
    436545, //Manuel Osorio Manrique de Zuñiga, Goya x
    437526, //A Forest at Dawn with a Deer Hunt, Peter Paul Rubens x
    437316, //Côte des Grouettes, near Pontoise, Camille Pissarro x
@@ -16,26 +16,28 @@ const objectIds = [
    let Clicked = false;
 
    document.getElementById("refresh").addEventListener("click", newObject)
-   function newObject() {
+
+   async function newObject() {
       if (Clicked == false) {
          Clicked = !Clicked
-         document.getElementById("refresh").innerHTML = 'Refresh <img src="images/arrows-rotate-solid.png" alt="refresh-logo" id="refresh-icon">';
+         document.getElementById("refresh").innerHTML = 'Refresh <i class="fa fa-refresh"></i>';
       }    
       const randomObject = objectIds[Math.floor(Math.random() * objectIds.length)];
       console.log(randomObject);
       const baseUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
       const url = `${baseUrl}${randomObject}`;
-      fetch(url)
-         .then((response) => response.json())
-         .then((objectData) => {
-            displayObject(objectData);
-            console.log(objectData);
-         })
-         .catch((error) => console.error(error));
-   };
+      
+      try {
+         const response = await fetch(url)
+         const objectData = await response.json();
+         displayObject(objectData);
+      } catch (error) {
+         console.log(error);
+      }    
+   }
 
 
-   function displayObject(objectData) {
+   async function displayObject(objectData) {
       document.getElementById("object-image").src = objectData.primaryImage;
       document.getElementById("title").innerText = objectData.title;
       document.getElementById("artist").innerText = objectData.artistDisplayName;
@@ -44,14 +46,15 @@ const objectIds = [
       console.log(objectData.primaryImage);
 
       const booksUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(objectData.artistDisplayName)}&maxResults=3&key=AIzaSyDxqPLEilRQEFCfDar65LdO-_W5i_xEvVc`;
-      fetch(booksUrl)
-         .then((response) => response.json())
-         .then((bookData) => {
-            displayBook(bookData);
-            console.log(bookData);
-         })
-         .catch((error) => console.error(error)); 
-   };
+      
+      try {
+         const response = await fetch(booksUrl)
+         const bookData = await response.json();
+         displayBook(bookData);
+      } catch (error) {
+         console.log(error);
+      }
+   }
 
    function displayBook(bookData) {
       for (let i = 0; i < bookData.items.length; i++) {
